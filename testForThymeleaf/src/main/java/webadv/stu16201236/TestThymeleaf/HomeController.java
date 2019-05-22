dede7b7c091a93303f48e850c11c52c9b3ef4af7
S16201236 <1721463423@qq.com>
2019-05-22 17:19:02 +0800

    关于Thymeleaf的练习

---
+++ testForThymeleaf/src/main/java/webadv/stu16201236/TestThymeleaf/HomeController.java
@@ -0,0 +1,27 @@
+package webadv.stu16201236.TestThymeleaf;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	// inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+	
+    private List<String> tasks = Arrays.asList("Git", "Maven", "Gradle", "Spring", "Bootstrap", "jQuery", "MyBatis","Redis");
+    
+    @GetMapping("/")
+	public String index(Model model) {
+    	model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+		return "welcome";		
+	}
+}
+
---
+++ testForThymeleaf/src/main/java/webadv/stu16201236/TestThymeleaf/TestForThymeleafApplication.java
@@ -0,0 +1,13 @@
+package webadv.stu16201236.TestThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForThymeleafApplication.class, args);
+	}
+
+}
